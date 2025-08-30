286b27b915aecc07d597a94039c666bb2cc8dd44
guenthner <guenthner.jonathan@gmail.com>
2025-08-30 22:16:50 +0200

    Fixed import error

--- guenthner_xyz/views.py
+++ guenthner_xyz/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpRequest
+
+from general import default_render
+
+
 def error404(request: HttpRequest):
     return default_render(request, "root/404", {
         "title": "404 Page"
