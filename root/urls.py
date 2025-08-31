808c9158c78b40e29d0bde3b36e3e4a5033ee1a2
guenthner <guenthner.jonathan@gmail.com>
2025-08-31 11:51:50 +0200

    Added math about

--- root/urls.py
+++ root/urls.py
@@ -4,18 +4,23 @@ from . import views
 
 app_name = "root"
 
-creations_urls = [path("", views.creations, name="creations"),
-                  path("chat", views.chat, name="chat"),
-                  path("cowsay", views.cowsay, name="cowsay")]
+creations_urls = [path("", views.view_creations, name="creations"),
+                  path("chat", views.view_chat, name="chat"),
+                  path("cowsay", views.view_cowsay, name="cowsay")]
 
-pretty_urls = [path("", views.pretty, name="pretty"),
-               path("words", views.words, name="words"),
-               path("pictures", views.pictures, name="pictures"), ]
+pretty_urls = [path("", views.view_pretty, name="pretty"),
+               path("words", views.view_words, name="words"),
+               path("pictures", views.view_pictures, name="pictures"), ]
+
+math_urls = [path("", views.view_math, name="math"),
+             path("paper", views.view_paper, name="paper"),
+             path("mandelbrot", views.view_mandelbrot, name="mandelbrot"),
+             path("eratosthenes", views.view_eratosthenes, name="eratosthenes")]
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("math/", views.math, name="math"),
+    path("", views.view_index, name="index"),
+    path("math/", include(math_urls)),
     path("pretty/", include(pretty_urls)),
-    path("memes", views.memes, name="memes"),
+    path("memes", views.view_memes, name="memes"),
     path("creations/", include(creations_urls)),
 ]
--- root/views.py
+++ root/views.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 
-from django.http import HttpRequest, Http404
+from django.http import HttpRequest
 
 from general import default_render
 from general.cowsay import run_cowsay
@@ -9,30 +9,26 @@ from general.cowsay import run_cowsay
 server_root = Path("/var/www/guenthner.xyz")
 
 
-def index(request: HttpRequest):
+def view_index(request: HttpRequest):
     return default_render(request, "root/index.html", {
         "title": "Günthner's Webpage"
     })
 
 
-def math(request: HttpRequest):
-    raise Http404
-
-
-def pretty(request: HttpRequest):
+def view_pretty(request: HttpRequest):
     return default_render(request, "root/pretty/pretty.html", {
         "title": "Pretty Stuff I made"
     })
 
 
-def words(request: HttpRequest):
+def view_words(request: HttpRequest):
     return default_render(request, "root/pretty/words.html", {
         "title": "Pretty Words I wrote",
-        "docs": os.listdir(server_root / "documents" / "stories"),
+        "docs": os.listdir(server_root / "documents" / "stories2"),
     })
 
 
-def pictures(request: HttpRequest):
+def view_pictures(request: HttpRequest):
     return default_render(request, "root/pretty/pictures.html", {
         "title": "Pretty Pictures I made",
         "images": {"Sunlight.png": "A pretty abstract pictures with the theme sunlight",
@@ -42,27 +38,51 @@ def pictures(request: HttpRequest):
     })
 
 
-def memes(request: HttpRequest):
+def view_memes(request: HttpRequest):
     return default_render(request, "root/memes.html", {
         "title": "Memes",
         "images": os.listdir(server_root / "images" / "memes"),
     })
 
 
-def creations(request: HttpRequest):
+def view_creations(request: HttpRequest):
     return default_render(request, "root/creations/creations.html", {
         "title": "Creations"
     })
 
 
-def cowsay(request: HttpRequest):
+def view_cowsay(request: HttpRequest):
     return default_render(request, "root/creations/cowsay.html", {
         "title": "Cowsay",
         "output": run_cowsay()
     })
 
 
-def chat(request: HttpRequest):
+def view_chat(request: HttpRequest):
     return default_render(request, "root/creations/chat.html", {
         "title": "Chat"
+    })
+
+
+def view_math(request: HttpRequest):
+    return default_render(request, "root/math/math.html", {
+        "title": "The Math-Corner"
+    })
+
+
+def view_paper(request: HttpRequest):
+    return default_render(request, "root/math/paper.html", {
+        "title": "Paper"
+    })
+
+
+def view_mandelbrot(request: HttpRequest):
+    return default_render(request, "root/math/mandelbrot.html", {
+        "title": "Mandelbrot"
+    })
+
+
+def view_eratosthenes(request: HttpRequest):
+    return default_render(request, "root/math/eratosthenes.html", {
+        "title": "Eratosthenes"
     })
