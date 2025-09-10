551da4fb57dac730e0d94ee00543fc332685553a
guenthner <guenthner.jonathan@gmail.com>
2025-09-10 19:29:57 +0200

    Patched security vulnerability in headers view

--- dev/views.py
+++ dev/views.py
@@ -1,10 +1,7 @@
-import json
 from pathlib import Path
 
 from django.contrib.auth.decorators import login_required, permission_required
-from django.core.exceptions import PermissionDenied
 from django.http import HttpRequest, HttpResponse
-from django.views.decorators.csrf import csrf_exempt
 
 from general import default_render
 
@@ -30,7 +27,8 @@ def view_log(request: HttpRequest, service: str, name: str):
     return HttpResponse(content, content_type="text/plain; charset=utf-8")
 
 
-@csrf_exempt
+@login_required
+@permission_required("root.admin")
 def view_headers(request: HttpRequest):
     params = {}
 
@@ -39,10 +37,6 @@ def view_headers(request: HttpRequest):
     for key in request.POST.keys():
         params[key] = request.POST[key]
 
-
-    if params["pwd"] != "fml as it sucks!":
-        raise PermissionDenied
-
     result = "Headers:\n"
 
     for name, value in request.headers.items():
@@ -50,7 +44,7 @@ def view_headers(request: HttpRequest):
 
     result += "\nMeta:\n"
 
-    for name,value in request.META.items():
+    for name, value in request.META.items():
         result += f"{name}: {value}\n"
 
     return HttpResponse(result, content_type="text/plain; charset=utf-8")
