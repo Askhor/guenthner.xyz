7e755cff34e34c42d07909354cd917db2fa71e4a
guenthner <guenthner.jonathan@gmail.com>
2025-09-10 16:13:29 +0200

    Added mariadb as database

--- guenthner_xyz/settings.py
+++ guenthner_xyz/settings.py
@@ -75,12 +75,26 @@ WSGI_APPLICATION = "guenthner_xyz.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+"Password For Django And guenthner.xyz"
+
+if DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.mariadb",
+            "NAME": "guenthner_xyz",
+            "USER": "guenthner_xyz",
+            "PASSWORD": os.environ["DB_PASSWORD"],
+            "HOST": "localhost",
+            "PORT": "",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
