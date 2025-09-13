fcfa5b53c74c97349a6d7d9146c628c13838a5d1
guenthner <guenthner.jonathan@gmail.com>
2025-09-13 18:39:10 +0200

    Added CACHES Option in settings.py

--- guenthner_xyz/settings.py
+++ guenthner_xyz/settings.py
@@ -34,7 +34,7 @@ if not DEBUG:
         "disable_existing_loggers": False,
         "formatters": {
             "default": {
-                "format": "{asctime} [{levelname:>5}] {name:>5}-{module:<10}: {message}",
+                "format": "{asctime} [{levelname:>5}] {name:>5}/{module:<10}: {message}",
                 "style": "{",
                 "datefmt": "%W %a %I:%M",
             }
@@ -63,6 +63,13 @@ if not DEBUG:
                 "level": "DEBUG",
                 "propagate": False,
             }
+        }
+    }
+    
+    CACHES = {
+        "default": {
+            "BACKEND": "django.core.cache.backends.db.DatabaseCache",
+            "LOCATION": "my_cache_table",
         }
     }
 
